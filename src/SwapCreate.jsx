c59f3b411bff2e167901531941258b325ea93641
Casey Bowman <case.bowman@gmail.com>
2023-03-22 04:24:22 -0500

    Add node props to swap props

--- src/SwapCreate.jsx
+++ src/SwapCreate.jsx
@@ -20,19 +20,66 @@ function SwapCreate({setSwapId, setSwapHash, setSecretSeekerId, setSecretHolderI
                 authorization: `Basic ${Buffer.from(creds).toString('base64')}`
             },
             body: JSON.stringify({
+
                 holderSubmarineSwapProps: {
                     uid: 'alice',
                     hash: null,
                     party: 'secretHolder',
                     quantity: baseQuantity,
-                    asset: 'BTC'
+                    asset: 'BTC',
+                    templateProps: {
+                        "version": "v2",
+                        "nodes": {
+                            "submarine": [{
+                                "nodetype": "lnd",
+                                "network": "lightning",
+                                "fungible": true,
+                                "asset": "BTC",
+                                "socket": "localhost:10001",
+                                "cert": "LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUNKekNDQWN5Z0F3SUJBZ0lRYjY4Um5LUUZxMUxDaFB0TFZXSXRrekFLQmdncWhrak9QUVFEQWpBeE1SOHcKSFFZRFZRUUtFeFpzYm1RZ1lYVjBiMmRsYm1WeVlYUmxaQ0JqWlhKME1RNHdEQVlEVlFRREV3VmhiR2xqWlRBZQpGdzB5TXpBek1ERXdOVFUxTURWYUZ3MHlOREEwTWpVd05UVTFNRFZhTURFeEh6QWRCZ05WQkFvVEZteHVaQ0JoCmRYUnZaMlZ1WlhKaGRHVmtJR05sY25ReERqQU1CZ05WQkFNVEJXRnNhV05sTUZrd0V3WUhLb1pJemowQ0FRWUkKS29aSXpqMERBUWNEUWdBRVB4ZGQ0NXFBQ1dYblZaRVJ6OHR1UHkzUTZUbnkxazZZd1dML0NwRkk2Y3VDVjVGUwo4TXZQRHlJQjZaRnVRQ1pnT1NrNlBaQXJMV2VLZ3F0OVdoelJ5cU9CeFRDQndqQU9CZ05WSFE4QkFmOEVCQU1DCkFxUXdFd1lEVlIwbEJBd3dDZ1lJS3dZQkJRVUhBd0V3RHdZRFZSMFRBUUgvQkFVd0F3RUIvekFkQmdOVkhRNEUKRmdRVVVINzNBMWFybjBTa05zMUFxQjlFVzJSUkt2c3dhd1lEVlIwUkJHUXdZb0lGWVd4cFkyV0NDV3h2WTJGcwphRzl6ZElJRllXeHBZMldDRG5CdmJHRnlMVzQwTFdGc2FXTmxnZ1IxYm1sNGdncDFibWw0Y0dGamEyVjBnZ2RpCmRXWmpiMjV1aHdSL0FBQUJoeEFBQUFBQUFBQUFBQUFBQUFBQUFBQUJod1NzRXdBQ01Bb0dDQ3FHU000OUJBTUMKQTBrQU1FWUNJUURFS2JiSW9VQkp3ZzljdThjelc4alBMZXJpZklSS3MvUWFEQXNLRndqdjRRSWhBUDA1WmtWWgpWWGpwaGVOWDRRVzB0Vm5mTXdxbzZnM0NlZDZsWUxSeXNuN1UKLS0tLS1FTkQgQ0VSVElGSUNBVEUtLS0tLQo=",
+                                "admin": "AgEDbG5kAvgBAwoQNE2Aa/CUlX94ecZNbRnHTxIBMBoWCgdhZGRyZXNzEgRyZWFkEgV3cml0ZRoTCgRpbmZvEgRyZWFkEgV3cml0ZRoXCghpbnZvaWNlcxIEcmVhZBIFd3JpdGUaIQoIbWFjYXJvb24SCGdlbmVyYXRlEgRyZWFkEgV3cml0ZRoWCgdtZXNzYWdlEgRyZWFkEgV3cml0ZRoXCghvZmZjaGFpbhIEcmVhZBIFd3JpdGUaFgoHb25jaGFpbhIEcmVhZBIFd3JpdGUaFAoFcGVlcnMSBHJlYWQSBXdyaXRlGhgKBnNpZ25lchIIZ2VuZXJhdGUSBHJlYWQAAAYgJVfaIBLpb7I6NyGHB5/c0Km5iQUS+zsB6DT1rLkxwUI=",
+                                "invoice": "AgEDbG5kAlgDChAyTYBr8JSVf3h5xk1tGcdPEgEwGhYKB2FkZHJlc3MSBHJlYWQSBXdyaXRlGhcKCGludm9pY2VzEgRyZWFkEgV3cml0ZRoPCgdvbmNoYWluEgRyZWFkAAAGIIbXb05hbtr+uz+a6Fy2WJUlSG3TIqDblUAMs6Gma3d8"
+                            }, {
+                                "nodetype": "bitcoind",
+                                "network": "bitcoin",
+                                "fungible": true,
+                                "asset": "BTC",
+                                "rpcuser": "",
+                                "rpcpassword": "",
+                                "rpcport": 8332
+                            }]
+                        },
+                    }
                 },
                 seekerSubmarineSwapProps: {
                     uid: 'carol',
                     hash: null,
                     party: 'secretSeeker',
                     quantity: baseQuantity,
-                    asset: 'BTC'
+                    asset: 'BTC',
+                    templateProps: {
+                        "version": "v2",
+                        "nodes": {
+                            "submarine": [{
+                                "nodetype": "lnd",
+                                "network": "lightning",
+                                "fungible": true,
+                                "asset": "BTC",
+                                "socket": "localhost:<port>",
+                                "cert": "",
+                                "admin": "",
+                                "invoice": ""
+                            }, {
+                                "nodetype": "bitcoind",
+                                "network": "bitcoin",
+                                "fungible": true,
+                                "asset": "BTC",
+                                "rpcuser": "",
+                                "rpcpassword": "",
+                                "rpcport": 8332
+                            }],
+                        }
+                    }
                 }
             })
         })
