c2ab5c0fced2386f8c3fe872171e3428e802fdba
victorwu <victorwu@users.noreply.github.com>
2023-05-16 13:37:52 -0400

    feature: crypto functions

--- src/ordinal/OrderForm.jsx
+++ src/ordinal/OrderForm.jsx
@@ -3,6 +3,7 @@ import { Buffer } from "buffer";
 import { useAppSelector } from '../store/hooks'
 // import {createHash, randomBytes} from "crypto"
 import getUser from '../store/util'
+import { hashSecret, newSecret } from '../utils/crypto.js'
 
 
 function OrderForm({setSwapId, setSwapHash, participant, setSecretHolderId, setSecretSeekerId, setSecret}) {
@@ -23,14 +24,14 @@ function OrderForm({setSwapId, setSwapHash, participant, setSecretHolderId, setS
     const user = getUser(users, participant)
 
 
-    const onClickPlaceOrder = () => {
+    const onClickPlaceOrder = async () => {
         const side = participant.username === 'alice'? 'ask' : 'bid'
 
-        const randomSecret = () => crypto.randomBytes(32)
-        const sha256 = buffer => crypto.createHash('sha256').update(buffer).digest('hex')
-        const secret = randomSecret()
-        const swapHash = sha256(secret)
-        const swapSecret = secret.toString('hex')
+        const secret = await newSecret() ;
+        console.log("new secret")
+        console.log(secret)
+        const swapHash = await hashSecret(secret)
+        console.log("new secret hash: " + swapHash)
 
         fetch('/api/v2/orderbook/limit', {
             method: 'PUT',
--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,12 +1,13 @@
-const hashSecret = async function hash(bytes) {
+export const hashSecret = async function hash(bytes) {
     const hashBuffer = await crypto.subtle.digest('SHA-256', bytes);
-    console.log('hashBuffer', hashBuffer)
+    // console.log('hashBuffer', hashBuffer)
     const hashArray = Array.from(new Uint8Array(hashBuffer));
-    console.log('hashArray', hashArray)
+    // console.log('hashArray', hashArray)
     const hashHex = hashArray
         .map(bytes => bytes.toString(16).padStart(2, '0'))
         .join('');
-    console.log('hashHex', hashHex);
+    // console.log('hashHex', hashHex);
+
     // log("hashSecret output utf8", utf8);
     // log("hashSecret output hashBuffer", hashBuffer);
     // log("hashSecret output hashArray", hashArray);
@@ -14,17 +15,19 @@ const hashSecret = async function hash(bytes) {
     return hashHex;
 }
 
-const secret = crypto.getRandomValues(new Uint8Array(32))
-const secretHex = [...secret]
+export const newSecret = async () => { return crypto.getRandomValues(new Uint8Array(32)) }
+
+// const secret = newSecret;
+export const secretHex = async (secret) => { [...secret]
     .map(byte => byte.toString(16).padStart(2, '0'))
-    .join('')
-console.log('secret', secret)
-console.log('secretHex', secretHex)
+    .join('')}
+// console.log('secret', secret)
+// console.log('secretHex(secret)', secretHex(secret))
 // const secret = Math.random().toString(36).slice(2);
 
 // log("{secret, secretHash, secret256}",{secret, secretHash: await hashSecret(secret)});
-const secretHash = await hashSecret(secret);
-console.log('secretHash', secretHash)
+// const secretHash = await hashSecret(secret);
+// console.log('secretHash', secretHash)
 //
 // setSecret(secretHex);
 // setOrderSecret(secretHash);
